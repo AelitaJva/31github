c80121adf5325e022892fc41d1a9fd0e1cd284e6
Aelita Jelden <nyc.aelita@gmail.com>
2023-11-24 15:48:28 +0600

    ArrayList Remove method practice

---
+++ src/com/java/WrapperClass/RemoveMethodExercise3.java
@@ -0,0 +1,30 @@
+package com.java.WrapperClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class RemoveMethodExercise3 {
+
+    public static void main(String[] args) {
+        List<String> guests = new ArrayList<>(Arrays.asList("Alice", "Bob", "Charlie"));
+        String checkedOutGuest = "Bob";
+
+        System.out.println(checkOutGuests(guests, checkedOutGuest));
+    }
+
+    public static List<String> checkOutGuests(List<String> guests, String guestName) {
+
+        for (int i = 0; i < guests.size(); i++) {
+            String currentGuestName = guests.get(i);
+
+            if (currentGuestName.equals(guestName)) {
+                guests.remove(i);
+                i--;
+            }
+        }
+
+        return guests;
+    }
+}
