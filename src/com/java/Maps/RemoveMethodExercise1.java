d6ba9b5af038e762e703328c5046d2b79b44657d
Aelita Jelden <nyc.aelita@gmail.com>
2023-11-30 16:50:50 +0600

    Map remove method

---
+++ src/com/java/Maps/RemoveMethodExercise1.java
@@ -0,0 +1,35 @@
+package com.java.Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class RemoveMethodExercise1 {
+    public static void main(String[] args) {
+        String inputString = "1 2 3 4 5 2 4 6 0 -3 -5 0 5";
+        System.out.println(classifyAndDuplicateIntegers(inputString));
+    }
+
+    public static String classifyAndDuplicateIntegers(String str) {
+        Map<Integer, String> newMap = new HashMap<>();
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            int strNum = Integer.parseInt(word);
+
+            if (!newMap.containsKey(strNum)) {
+                if (strNum > 0 && strNum % 2 == 0) {
+                    newMap.put(strNum, "Positive Even");
+                } else if (strNum > 0 && strNum % 2 != 0) {
+                    newMap.put(strNum, "Positive Odd");
+                } else if (strNum == 0) {
+                    newMap.put(strNum, "Zero");
+                } else {
+                    newMap.put(strNum, "Negative");
+                }
+            }
+        }
+        return newMap.toString();
+    }
+}
+
---
+++ src/com/java/Maps/RemoveMethodExercise2.java
@@ -0,0 +1,9 @@
+package com.java.Maps;
+
+public class RemoveMethodExercise2 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
