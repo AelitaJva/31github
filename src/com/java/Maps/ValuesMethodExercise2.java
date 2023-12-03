14bab8ff3ce724bb6156fa236bb0a4e0d6a83edb
Aelita Jelden <nyc.aelita@gmail.com>
2023-12-03 15:35:39 +0600

    Map Values(); Method & practice

---
+++ src/com/java/Maps/ValuesMethodExercise2.java
@@ -0,0 +1,27 @@
+package com.java.Maps;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValuesMethodExercise2 {
+    public static void main(String[] args) {
+        Map<String, Integer> inventory = new HashMap<>(Map.of("Apples", 50, "Bananas",30, "Oranges",40));
+
+        int result = calculateTotalItems(inventory);
+        System.out.println(result);
+
+    }
+
+    public static int calculateTotalItems (Map<String, Integer> inventory) {
+
+        Collection<Integer> quantities = inventory.values();
+
+        int sum = 0;
+        for(Integer quantity : quantities) {
+            sum+=quantity;
+        }
+
+        return sum;
+    }
+}
