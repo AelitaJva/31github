e606e487589d9d18f4c99426c6ff0a4f93c5bf2f
Aelita Jelden <nyc.aelita@gmail.com>
2023-12-06 22:22:17 +0600

    Consturtor with multiple params, cons overriding,this keyword, cons chaining: this(); keyword

---
+++ src/com/java/Week12_OOP/Investment.java
@@ -0,0 +1,22 @@
+package com.java.Week12_OOP;
+
+class Investment {
+    public String investmentId;
+    public String investmentName;
+    public double initialInvestment;
+    public double currentValue;
+    public boolean isProfitable;
+
+
+    public Investment () {
+
+    }
+
+    public Investment (String id, String name, double initial, double current, boolean isProfitableParam) {
+        investmentId = id;
+        investmentName = name;
+        initialInvestment = initial;
+        currentValue = current;
+        isProfitable = isProfitableParam;
+    }
+}
