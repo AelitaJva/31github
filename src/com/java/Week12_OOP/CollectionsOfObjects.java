b495fc42e103bd44e7481db2fc0deae9610bb53e
Aelita Jelden <nyc.aelita@gmail.com>
2023-12-08 22:47:58 +0600

    Collection of Custom Object & practice

---
+++ src/com/java/Week12_OOP/CollectionsOfObjects.java
@@ -0,0 +1,87 @@
+package com.java.Week12_OOP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsOfObjects {
+    public static void main(String[] args) {
+        // open the list of object first
+        // create multiple objects of class
+        // add all objects to the list
+
+        // 1.
+        List<BankingAccount> allAccounts = new ArrayList<>();
+
+
+        BankingAccount ba1 = new BankingAccount("Chase", 245697);
+        BankingAccount ba2 = new BankingAccount("BOA", 475927523);
+        BankingAccount ba3 = new BankingAccount("US Bank", 1234567);
+        BankingAccount ba4 = new BankingAccount("Chase", 245697);
+
+
+        // 2.
+        allAccounts.add(ba1);
+        allAccounts.add(ba2);
+        allAccounts.add(ba3);
+        System.out.println(allAccounts);
+
+
+        // 3.
+        for (BankingAccount obj : allAccounts) {
+            if (obj.getName().equalsIgnoreCase("chase")) {
+                System.out.println(obj.getName());
+            }
+        }
+
+        long result = BankingAccount.calculateTotalBalanceOfAllAccount(allAccounts);
+        System.out.println(result);
+        // in collectionsOfObjects class create a static method called calculateTotalBalanceOfAllAccount
+        // that takes a list of BankingAccounts as a param
+        // and it sums up balance of every account
+        // and return the total balance of all accounts
+
+    }
+
+
+}
+
+// no classes with duplicated names within the same package
+// POJO - plain old java object (get, set methods)
+// creating object store the data
+class BankingAccount {
+
+    private String name;
+    private double balance;
+
+    public BankingAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public static long calculateTotalBalanceOfAllAccount(List<BankingAccount> allAccounts) {
+        long totalBalanceOfAllAccounts = 0;
+
+        for (BankingAccount account : allAccounts) {
+            totalBalanceOfAllAccounts += account.getBalance();
+        }
+        return totalBalanceOfAllAccounts;
+    }
+
+
+}
---
+++ src/com/java/Week12_OOP/CollectionsOfObjectsExercise2.java
@@ -0,0 +1,156 @@
+package com.java.Week12_OOP;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionsOfObjectsExercise2 {
+    public static void main(String[] args) {
+
+        Kitchen kitchen = new Kitchen();
+
+        Veges tomato = new Veges("Tomato", "2023-06-24", "2023-06-30");
+        Veges cucumber = new Veges("Cucumber", "2023-06-25", "2023-07-01");
+
+        Fruits apple = new Fruits("Apple", "2023-06-24", "2023-07-01");
+        Fruits banana = new Fruits("Banana", "2023-06-25", "2023-07-02");
+
+        kitchen.addVegetable(tomato, 2.5);
+        kitchen.addVegetable(cucumber, 1.8);
+        kitchen.addFruit(apple, 3.0);
+        kitchen.addFruit(banana, 2.0);
+
+        // Print the inventory of vegetables in the Kitchen object
+        System.out.println("Vegetable Inventory:");
+        Map<Veges, Double> vegeInventory = kitchen.getVegesInventory();
+        for (Map.Entry<Veges, Double> entry : vegeInventory.entrySet()) {
+            Veges vege = entry.getKey();
+            double weight = entry.getValue();
+            System.out.println("- Name: " + vege.getName());
+            System.out.println("  Weight: " + weight + " kg");
+            System.out.println("  Arrival Time: " + vege.getArrivalTime());
+            System.out.println("  Expire Date: " + vege.getExpireDate());
+        }
+
+// Print the inventory of fruits in the Kitchen object
+        System.out.println("\nFruit Inventory:");
+        Map<Fruits, Double> fruitInventory = kitchen.getFruitsInventory();
+        for (Map.Entry<Fruits, Double> entry : fruitInventory.entrySet()) {
+            Fruits fruit = entry.getKey();
+            double weight = entry.getValue();
+            System.out.println("- Name: " + fruit.getName());
+            System.out.println("  Weight: " + weight + " kg");
+            System.out.println("  Arrival Time: " + fruit.getArrivalTime());
+            System.out.println("  Expire Date: " + fruit.getExpireDate());
+        }
+
+// Remove a vegetable and a fruit from the inventory
+        kitchen.removeVegetable(tomato);
+        kitchen.removeFruit(apple);
+
+// Print the updated inventory of vegetables and fruits in the Kitchen object
+        System.out.println("\nUpdated Vegetable Inventory:");
+        vegeInventory = kitchen.getVegesInventory();
+        for (Map.Entry<Veges, Double> entry : vegeInventory.entrySet()) {
+            Veges vege = entry.getKey();
+            double weight = entry.getValue();
+            System.out.println("- Name: " + vege.getName());
+            System.out.println("  Weight: " + weight + " kg");
+            System.out.println("  Arrival Time: " + vege.getArrivalTime());
+            System.out.println("  Expire Date: " + vege.getExpireDate());
+        }
+
+        System.out.println("\nUpdated Fruit Inventory:");
+        fruitInventory = kitchen.getFruitsInventory();
+        for (Map.Entry<Fruits, Double> entry : fruitInventory.entrySet()) {
+            Fruits fruit = entry.getKey();
+            double weight = entry.getValue();
+            System.out.println("- Name: " + fruit.getName());
+            System.out.println("  Weight: " + weight + " kg");
+            System.out.println("  Arrival Time: " + fruit.getArrivalTime());
+            System.out.println("  Expire Date: " + fruit.getExpireDate());
+        }
+    }
+}
+
+class Kitchen {
+    private Map<Veges, Double> vegesInventory;
+    private Map<Fruits, Double> fruitsInventory;
+
+    public Kitchen() {
+        this.vegesInventory = new HashMap<>();
+        this.fruitsInventory = new HashMap<>();
+    }
+
+    public void addVegetable(Veges vege, double weight) {
+        vegesInventory.put(vege, weight);
+    }
+
+    public void addFruit(Fruits fruit, double weight) {
+        fruitsInventory.put(fruit, weight);
+    }
+
+    public void removeVegetable(Veges vege) {
+        vegesInventory.remove(vege);
+    }
+
+    public void removeFruit(Fruits fruit) {
+        fruitsInventory.remove(fruit);
+    }
+
+    public Map<Veges, Double> getVegesInventory() {
+        return vegesInventory;
+    }
+
+
+    public Map<Fruits, Double> getFruitsInventory() {
+        return fruitsInventory;
+    }
+}
+
+class Veges {
+    private String name;
+    private final String arrivalTime;
+    private final String expireDate;
+
+    public Veges(String name, String arrivalTime, String expireDate) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.expireDate = expireDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+}
+
+class Fruits {
+    private String name;
+    private final String arrivalTime;
+    private final String expireDate;
+
+    public Fruits(String name, String arrivalTime, String expireDate) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.expireDate = expireDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+}
